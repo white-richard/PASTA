95f1dfb0ea58537171a236421059e7af1c6cc51b
white-richard <rich.white285@yahoo.com>
2026-04-10 16:39:48 -0700

    embed text using siglip text encoder

---
+++ src/descript_embed.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+from collections import defaultdict
+
+os.environ["USE_TF"] = "0"
+
+import pathlib
+
+import torch
+from tqdm import tqdm
+
+torch.cuda.is_available()
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--encoder",
+    choices=["bert", "siglip"],
+    default="siglip",
+    help="Text encoder to use: 'bert' (bert-base-cased) or 'siglip' (google/siglip-so400m-patch14-384)",
+)
+args = parser.parse_args()
+
+path = pathlib.Path("datasets/text_phoenix-descript")
+
+train = torch.load(path / "phoenix_SLdescriptions.train", weights_only=False)
+dev = torch.load(path / "phoenix_SLdescriptions.dev", weights_only=False)
+test = torch.load(path / "phoenix_SLdescriptions.test", weights_only=False)
+
+print(len(train))
+print(len(dev))
+print(len(test))
+
+device = torch.device("cuda:0")
+
+if args.encoder == "siglip":
+    from transformers import SiglipTextModel, SiglipTokenizer
+
+    SIGLIP_MODEL_ID = "google/siglip-so400m-patch14-384"
+    tokenizer = SiglipTokenizer.from_pretrained(SIGLIP_MODEL_ID)
+    model = SiglipTextModel.from_pretrained(SIGLIP_MODEL_ID)
+    feat_key = "siglip_feat"
+
+    def encode(texts):
+        inputs = tokenizer(
+            texts,
+            return_tensors="pt",
+            padding="max_length",
+            max_length=64,
+            truncation=True,
+        )
+        with torch.no_grad():
+            outputs = model(input_ids=inputs["input_ids"].to(device))
+        return outputs.pooler_output.cpu()
+
+else:
+    from transformers import BertModel, BertTokenizer
+
+    tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
+    model = BertModel.from_pretrained("bert-base-cased")
+    feat_key = "bert_feat"
+
+    def encode(texts):
+        inputs = tokenizer(texts, return_tensors="pt", padding=True, truncation=True)
+        with torch.no_grad():
+            outputs = model(
+                input_ids=inputs["input_ids"].to(device),
+                attention_mask=inputs["attention_mask"].to(device),
+            )
+        return outputs.last_hidden_state[:, 0, :].cpu()
+
+
+model.to(device)
+
+new_train = defaultdict(dict)
+new_dev = defaultdict(dict)
+new_test = defaultdict(dict)
+
+for k, v in tqdm(train.items()):
+    new_train[k]["texts"] = v
+    new_train[k][feat_key] = encode(v)
+
+for k, v in tqdm(dev.items()):
+    new_dev[k]["texts"] = v
+    new_dev[k][feat_key] = encode(v)
+
+for k, v in tqdm(test.items()):
+    new_test[k]["texts"] = v
+    new_test[k][feat_key] = encode(v)
+
+path = pathlib.Path("out") / path
+torch.save(new_train, path / f"phoenix_SLdescriptions_{args.encoder}_train.pt")
+torch.save(new_dev, path / f"phoenix_SLdescriptions_{args.encoder}_dev.pt")
+torch.save(new_test, path / f"phoenix_SLdescriptions_{args.encoder}_test.pt")
