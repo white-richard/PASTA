e2275c852230c1b8f0ad3c007bc0197cbc2f5ef6
white-richard <rich.white285@yahoo.com>
2026-05-02 21:40:15 -0700

    Create eval_author_mmslt.bash

---
+++ scripts/eval_author_mmslt.bash
@@ -0,0 +1,11 @@
+.venv/bin/python src/train_mmslt.py \
+    --eval \
+    --resume best_checkpoint_phoenix.pth \
+    --language_decoder mbart \
+    --vision_backbone resnet18 \
+    --config src/configs/config_mmslt_phoenix.yaml \
+    --output_dir out/mmslt_eval \
+    --eval-metrics \
+    --num_workers 4 \
+    --eval_num_workers 2 \
+    --batch-size 16
